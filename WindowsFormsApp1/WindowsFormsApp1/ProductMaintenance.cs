70c598499e74114137d39fe6481f2cfbccceea93
KingsleySepeng <91886868+KingsleySepeng@users.noreply.github.com>
2022-10-04 09:52:56 +0200

    Classes

---
+++ WindowsFormsApp1/WindowsFormsApp1/ProductMaintenance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class ProductMaintenance:DataHandler
+    {
+        float MoneySpent;
+        float MoneyEarned;
+        int NumberClients;
+        float profit;
+
+        public float MoneySpent1 { get => MoneySpent; set => MoneySpent = value; }
+        public float MoneyEarned1 { get => MoneyEarned; set => MoneyEarned = value; }
+        public int NumberClients1 { get => NumberClients; set => NumberClients = value; }
+        public float Profit { get => profit; set => profit = value; }
+
+        public override void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void update()
+        {
+            throw new NotImplementedException();
+        }
+  
+    }
+}
